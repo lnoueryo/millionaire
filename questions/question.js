6225d27fbf943f22f13cd14bf9dad40b0b14a30d
lnoueryo <jounetsism@gmail.com>
2023-08-17 23:17:12 +0900

    first commit

---
+++ questions/question.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        "id": 1, "question": "福岡県の人口(7/1時点)", "image": "", "selects": [
+            { "id": 1, "description": "5107977" },
+            { "id": 2, "description": "7・7・7" },
+            { "id": 3, "description": "500万人くらい" },
+            { "id": 4, "description": "5207977" },
+        ], "answer": 1
+    },
+    {
+        "id": 2, "question": "address v125の燃料タンク容量", "image": "", "selects": [
+            { "id": 1, "description": "7・7・7" },
+            { "id": 2, "description": "6.1" },
+            { "id": 3, "description": "6.000000001" },
+            { "id": 4, "description": "6" },
+        ], "answer": 4
+    },
+    {
+        "id": 3, "question": "ZRX400のリアタイヤサイズ", "image": "", "selects": [
+            { "id": 1, "description": "150/50R18" },
+            { "id": 2, "description": "150/60R18" },
+            { "id": 3, "description": "160/60R18" },
+            { "id": 4, "description": "150/60R17" },
+        ], "answer": 2
+    },
+    {
+        "id": 4, "question": "このホットプリンタの品番と本体重量", "image": "https://www.edm-net.co.jp/img/img_hp2600t.png", "selects": [
+            { "id": 1, "description": "HP2600T 7.4kg" },
+            { "id": 2, "description": "HP2500T 7.4kg" },
+            { "id": 3, "description": "HP2600T 7.2kg" },
+            { "id": 4, "description": "HP2600T 7.0kg" },
+        ], "answer": 1
+    },
+]
