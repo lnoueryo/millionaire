fab31cdd2d233bb1797dcb8faa498c3a039aad08
lnoueryo <jounetsism@gmail.com>
2023-08-18 13:23:30 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -2,58 +2,34 @@ let questionIndex = 0;
 let waiting = false;
 let score = 0;
 
-const displayQuestions = (i) => {
-    const question = getElement('question-container');
-    const selects = getElement('selects-container');
-
-    const questionDiv = document.createElement('div');
-    const questionText = document.createElement('p');
-    questionText.textContent = questions[i].question;
-    questionDiv.appendChild(questionText);
-    // 画像がある場合は表示
-    if (questions[i].image) {
-        const imageElement = document.createElement('img');
-        imageElement.src = questions[i].image;
-        questionDiv.appendChild(imageElement);
-    }
-    question.appendChild(questionDiv);
-    questions[i].selects.forEach(select => {
-        // 質問を表示するためのdivを作成
-        const selectDiv = document.createElement('div');
-        selectDiv.id = 'question-' + select.id;
-        selectDiv.className = 'select-item';
-
-        // 質問テキストを表示
-        const selectText = document.createElement('p');
-        selectText.textContent = select.description;
-        selectDiv.appendChild(selectText);
-
-        selectDiv.addEventListener('click', () => {
-            if(waiting) return;
-            console.debug(select.id)
-            waiting = true;
-            if(select.id == questions[i].answer) {
-                console.debug('正解');
-                selectDiv.classList.add('correct');
-                score ++;
-            } else {
-                const answer = document.getElementById('question-' + questions[i].answer);
-                answer.classList.add('correct');
-                selectDiv.classList.add('incorrect');
-            }
-            const answerButton = document.getElementById('answer-button');
-            answerButton.className = 'show';
-            updateScore(score);
-        })
-
-        selects.appendChild(selectDiv);
-    });
+const displayQuestion = (i) => {
+    const question = questions[i]
+    createContent(question)
+    createOptions(question)
 }
 
-const getElement = (id) => {
+const resetElement = (id) => {
     const el = document.getElementById(id);
     el.innerHTML = '';
     return el;
+}
+
+const onOptionClicked = (option, optionDiv) => {
+    const question = questions[questionIndex]
+    if(waiting) return;
+    console.debug(option.id)
+    waiting = true;
+    if(option.id == question.answer) {
+        console.debug('正解');
+        optionDiv.classList.add('correct');
+        score ++;
+    } else {
+        const answer = document.getElementById('option-' + question.answer);
+        answer.classList.add('correct');
+        optionDiv.classList.add('incorrect');
+    }
+    const answerButton = document.getElementById('answer-button');
+    answerButton.className = 'show';
 }
 
 const onNextQuestionClicked = () => {
@@ -61,20 +37,20 @@ const onNextQuestionClicked = () => {
     const answerButton = document.getElementById('answer-button')
     answerButton.className = 'hide';
     if(questionIndex == questions.length - 1) {
-        getElement('selects-container')
-        const question =  getElement('question-container');
-        const questionDiv = document.createElement('div');
+        resetElement('options-container')
+        const contentEl =  resetElement('content-container');
+        const contentDiv = document.createElement('div');
         const button = document.createElement('button');
         button.textContent = 'もう一回';
         button.addEventListener('click', () => {
           main()
         })
-        questionDiv.appendChild(button);
-        question.appendChild(questionDiv);
+        contentDiv.appendChild(button);
+        contentEl.appendChild(contentDiv);
         return;
     }
     questionIndex ++;
-    displayQuestions(questionIndex);
+    displayQuestion(questionIndex);
 }
 
 const updateScore = (score) => {
@@ -82,10 +58,54 @@ const updateScore = (score) => {
   scoreEl.textContent = score;
 }
 
+const createContent = (question) => {
+    const contentEl = resetElement('content-container');
+    const contentDiv = document.createElement('div');
+    const contentText = document.createElement('p');
+    contentText.textContent = question.content;
+    contentDiv.appendChild(contentText);
+    // 画像がある場合は表示
+    if (question.image) {
+        const imageElement = document.createElement('img');
+        imageElement.src = question.image;
+        contentDiv.appendChild(imageElement);
+    }
+    contentEl.appendChild(contentDiv);
+    return contentEl;
+}
+
+const createOptions = (question) => {
+    const optionsEl = resetElement('options-container');
+    question.options.forEach(option => {
+        const optionDiv = createOption(option)
+        optionDiv.addEventListener('click', () => {
+            onOptionClicked(option, optionDiv)
+            updateScore(score);
+        })
+
+        optionsEl.appendChild(optionDiv);
+    });
+
+    return optionsEl;
+}
+
+const createOption = (option) => {
+    // 質問を表示するためのdivを作成
+    const optionDiv = document.createElement('div');
+    optionDiv.id = 'option-' + option.id;
+    optionDiv.className = 'option-item';
+
+    // 質問テキストを表示
+    const optionText = document.createElement('p');
+    optionText.textContent = option.description;
+    optionDiv.appendChild(optionText);
+    return optionDiv;
+}
+
 const main = () => {
   questionIndex = 0;
   score = 0;
-  displayQuestions(questionIndex);
+  displayQuestion(questionIndex);
   updateScore(score);
 }
 
--- questions/question.js
+++ questions/question.js
@@ -1,34 +1,42 @@
 const questions = [
     {
-        "id": 1, "question": "福岡県の人口(7/1時点)", "image": "", "selects": [
+        "id": 1, "content": "福岡県の人口(7/1時点)", "image": "", "options": [
             { "id": 1, "description": "5107977" },
             { "id": 2, "description": "7・7・7" },
             { "id": 3, "description": "500万人くらい" },
             { "id": 4, "description": "5207977" },
-        ], "answer": 1
+        ],
+        "answer": 1,
+        "explanation": "福岡県の公式の統計情報によれば、人口は約5107977人です。"
     },
     {
-        "id": 2, "question": "address v125の燃料タンク容量", "image": "", "selects": [
+        "id": 2, "content": "address v125の燃料タンク容量", "image": "", "options": [
             { "id": 1, "description": "7・7・7" },
             { "id": 2, "description": "6.1" },
             { "id": 3, "description": "6.000000001" },
             { "id": 4, "description": "6" },
-        ], "answer": 4
+        ],
+        "answer": 4,
+        "explanation": "福岡県の公式の統計情報によれば、人口は約5107977人です。"
     },
     {
-        "id": 3, "question": "ZRX400のリアタイヤサイズ", "image": "", "selects": [
+        "id": 3, "content": "ZRX400のリアタイヤサイズ", "image": "", "options": [
             { "id": 1, "description": "150/50R18" },
             { "id": 2, "description": "150/60R18" },
             { "id": 3, "description": "160/60R18" },
             { "id": 4, "description": "150/60R17" },
-        ], "answer": 2
+        ],
+        "answer": 2,
+        "explanation": "福岡県の公式の統計情報によれば、人口は約5107977人です。"
     },
     {
-        "id": 4, "question": "このホットプリンタの品番と本体重量", "image": "https://www.edm-net.co.jp/img/img_hp2600t.png", "selects": [
+        "id": 4, "content": "このホットプリンタの品番と本体重量", "image": "https://www.edm-net.co.jp/img/img_hp2600t.png", "options": [
             { "id": 1, "description": "HP2600T 7.4kg" },
             { "id": 2, "description": "HP2500T 7.4kg" },
             { "id": 3, "description": "HP2600T 7.2kg" },
             { "id": 4, "description": "HP2600T 7.0kg" },
-        ], "answer": 1
+        ],
+        "answer": 1,
+        "explanation": "福岡県の公式の統計情報によれば、人口は約5107977人です。"
     },
 ]
